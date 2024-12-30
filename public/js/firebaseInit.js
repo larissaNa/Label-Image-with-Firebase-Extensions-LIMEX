1ef955edfdabf86403d28a53b81358f1ba3a2638
larissaNa <larysou66@gmail.com>
2024-12-29 21:07:02 -0300

    Removendo SDK

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDPzU2WE44jrV8cbplMFsCv1y-5LFeO3qU",
-    authDomain: "uploadimg-fc170.firebaseapp.com",
-    projectId: "uploadimg-fc170",
-    storageBucket: "uploadimg-fc170.appspot.com",
-    messagingSenderId: "779865574756",
-    appId: "1:779865574756:web:64f2e8833bd6a6d50bbe71",
-    measurementId: "G-S2VR1JKHPY"
+    
     };
     
   // Inicializa o Firebase
